79477518a89b0fec270c0cd8b67f6fbe1f040406
Toma Milićević <tomatomamilicevic@gmail.com>
2025-04-18 18:32:28 +0200

    Header finished and iconify-icons import

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,29 @@
 import Navigation from "./Navigation";
+import { Icon } from "@iconify-icon/react";
 
 function Header({ isNavActive, setIsNavActive }) {
   return (
-    <header className="h-24! bg-red-500! w-full">
-      <button onClick={() => setIsNavActive(true)}>nav</button>
+    <>
       <Navigation isNavActive={isNavActive} setIsNavActive={setIsNavActive} />
-    </header>
+      <header className="h-24 bg-bordo-600 w-full flex items-center px-8 justify-between">
+        <div className="h-full flex items-center gap-4 ">
+          <Icon
+            onClick={() => setIsNavActive(true)}
+            icon="mdi:menu"
+            width="42"
+            height="42"
+            className=""
+          />
+          <Icon
+            icon="mdi:bell-outline"
+            width="36"
+            height="42"
+            className="pl-0"
+          />
+        </div>
+        <Icon icon="jam:search" width="40" height="40" />
+      </header>
+    </>
   );
 }
 
