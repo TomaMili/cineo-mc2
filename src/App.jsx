e68087e9b1c77002e8223b772adc3faf61dfccf0
Toma Milićević <tomatomamilicevic@gmail.com>
2025-04-18 13:25:50 +0200

    Changed routing and added setup for user auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PageNotFound from "./pages/PageNotFound";
 import HomePage from "./pages/HomePage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { useAuth } from "./hooks/useAuth";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,31 +15,61 @@ const queryClient = new QueryClient({
 });
 
 function App() {
+  // const { user } = useAuth();
+  const user = true;
+
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="homepage" />} />
-            <Route path="homepage" element={<HomePage />} />
-            <Route path="profile/:userId" element={<HomePage />} />
-            <Route path="profile/achievements" element={<HomePage />} />
-            <Route path="watchlist" element={<HomePage />} />
-            <Route path="watchlist/watchlater" element={<HomePage />} />
-            <Route path="watchlist/watched" element={<HomePage />} />
-            <Route path="watchlist/collections" element={<HomePage />} />
-            <Route path="watch-together" element={<HomePage />} />
-            <Route path="movie/:id" element={<HomePage />} />
-            <Route path="settings" element={<HomePage />} />
-            <Route path="settings/info-settings" element={<HomePage />} />
-            <Route path="settings/platforms-settings" element={<HomePage />} />
-            <Route path="settings/plan-settings" element={<HomePage />} />
-            <Route path="suggested" element={<HomePage />} />
-          </Route>
+          {!user ? (
+            <>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route element={<RegisterLayout />}>
+                <Route
+                  index
+                  element={<Navigate replace to="register/info" />}
+                />
+                <Route path="register/info" element={<RegisterInfo />} />
+                <Route path="register/genres" element={<RegisterGenres />} />
+                <Route
+                  path="register/platforms"
+                  element={<RegisterPlatforms />}
+                />
+                <Route path="register/actors" element={<RegisterActors />} />
+                <Route
+                  path="register/payment-plan"
+                  element={<RegisterPaymentPlan />}
+                />
+                <Route path="register/finish" element={<RegisterFinish />} />
+              </Route>
+            </>
+          ) : (
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate replace to="homepage" />} />
+              <Route path="homepage" element={<HomePage />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="profile/achievements" element={<Achievements />} />
+              <Route path="watchlater" element={<WatchLater />} />
+              <Route path="watched" element={<Watched />} />
+              <Route path="collections" element={<Collections />} />
+              <Route path="watch-together" element={<WatchTogether />} />
+              <Route
+                path="watch-together/:groupId"
+                element={<WatchTogetherGroup />}
+              />
+              <Route path="movie/:id" element={<MovieDetail />} />
+              <Route path="settings/info" element={<SettingsInfo />} />
+              <Route
+                path="settings/platforms"
+                element={<SettingsPlatforms />}
+              />
+              <Route path="settings/plan" element={<SettingsPlan />} />
+              <Route path="browse" element={<Browse />} />
+            </Route>
+          )}
           <Route path="*" element={<PageNotFound />} />
-          <Route path="landing-page" element={<PageNotFound />} />
-          <Route path="register" element={<PageNotFound />} />
-          <Route path="login" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
+// import { AuthProvider } from "./hooks/useAuth.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    {/* <AuthProvider> */}
     <App />
+    {/* </AuthProvider> */}
   </StrictMode>
 );
