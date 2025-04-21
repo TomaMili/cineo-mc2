81fccd24c84804f9361736e15d1f5955a9143a86
Toma Milićević <tomatomamilicevic@gmail.com>
2025-04-21 14:48:51 +0200

    Moved supabase key and url to .env file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://jxlxyqmonpgfsfsumyvv.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4bHh5cW1vbnBnZnNmc3VteXZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MTE1NTUsImV4cCI6MjA2MDQ4NzU1NX0.l8KHelRmrrLG6GRYCFnoxSIG0VMI77lQmR_DZZAApt8";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
