b13965899bc490fb136b7183e6002413676f9d28
Toma Milićević <tomatomamilicevic@gmail.com>
2025-05-16 17:33:53 +0200

    Add responsive background images in HeroSection with AVIF and WebP formats

--- src/features/homepage/HeroSection.jsx
+++ src/features/homepage/HeroSection.jsx
@@ -2,15 +2,28 @@ import { useSuperSuggest } from "../../context/SuperSuggestContext";
 
 function HeroSection() {
   const { show } = useSuperSuggest();
+
   return (
-    <div className="text-white">
-      <div className="flex flex-col items-center gap-6 z-0 bg-[url(/bg-image.jpg)] -mt-24 pb-28 ">
+    <div className="relative w-full text-white z-0 h-2/3">
+      <picture className="absolute inset-0 z-0 w-full h-full">
+        <source srcSet="/bg-image.jpg" type="image/jpg" />
+        <source srcSet="/bg-image.avif" type="image/avif" />
+        <source srcSet="/bg-image.webp" type="image/webp" />
+        <img
+          src="/bg-image.jpg"
+          alt="Background image"
+          className="h-full w-full object-cover"
+          decoding="async"
+          fetchPriority="high"
+        />
+      </picture>
+      <div className="flex flex-col items-center gap-6 z-1 -mt-24 pb-28 ">
         <img
           src="/logo-cineo.svg"
           alt="logo"
-          width="34%"
-          height="34%"
           className="relative mx-auto mt-34"
+          decoding="async"
+          fetchPriority="high"
         />
         <h2 className="z-0 text-6xl font-light text-center pt-20">
           Ready for Your Next <br /> Watch?
