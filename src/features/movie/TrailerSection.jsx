b991b1a4a46bc5ab8a6b21aa5a6d035f9133bc88
Toma Milićević <tomatomamilicevic@gmail.com>
2025-06-06 20:57:34 +0200

    Refactor TrailerSection for improved layout and responsive design

--- src/features/movie/TrailerSection.jsx
+++ src/features/movie/TrailerSection.jsx
@@ -2,9 +2,9 @@ function TrailerSection({ youtubeKey }) {
   if (!youtubeKey) return null;
 
   return (
-    <section className="flex flex-col items-center my-40 gap-6">
-      <h2 className="text-3xl font-medium">OFFICIAL TRAILER</h2>
-      <div className="w-2/3 aspect-video rounded-xl overflow-hidden mx-auto">
+    <section className="flex flex-col items-center my-20 lg:my-40 gap-6">
+      <h2 className="text-2xl lg:text-3xl font-medium">OFFICIAL TRAILER</h2>
+      <div className="w-9/10 lg:w-2/3 aspect-video rounded-xl overflow-hidden mx-auto">
         <iframe
           src={`https://www.youtube.com/embed/${youtubeKey}?rel=0`}
           title="Movie trailer"
