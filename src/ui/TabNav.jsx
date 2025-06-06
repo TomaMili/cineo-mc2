2fca36531004412792abfd532da8c1d9fa8d3e1b
Toma Milićević <tomatomamilicevic@gmail.com>
2025-06-07 00:17:21 +0200

    Adjust TabNav component for improved spacing and consistency in text alignment

--- src/ui/TabNav.jsx
+++ src/ui/TabNav.jsx
@@ -2,14 +2,14 @@ import { NavLink } from "react-router-dom";
 
 export default function TabNav({ tabs }) {
   return (
-    <nav className="flex bg-gray-500/40 rounded-full overflow-hidden h-9">
+    <nav className="flex bg-gray-500/40 rounded-full overflow-hidden h-8 sm:h-9">
       {tabs.map(([to, label]) => (
         <NavLink
           key={label}
           to={to}
           end={to === ""}
           className={({ isActive }) =>
-            `flex-1 text-center leading-9 transition-colors text-md
+            `flex-1 text-center leading-8 sm:leading-9 transition-colors text-md
              ${
                isActive
                  ? "bg-bordo-500 text-white"
