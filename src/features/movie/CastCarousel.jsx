33ede5435d8937ae2f5d66f41b6d5e8d170e9c00
Toma Milićević <tomatomamilicevic@gmail.com>
2025-06-06 20:57:24 +0200

    Add responsive behavior to CastCarousel and improve CastSection layout

--- src/features/movie/CastCarousel.jsx
+++ src/features/movie/CastCarousel.jsx
@@ -5,6 +5,7 @@ import usePrevNextButtons, {
   PrevButton,
   NextButton,
 } from "../../ui/MovieCarouselArrowButton";
+import { useEffect, useState } from "react";
 
 export default function CastCarousel({
   slides = [],
@@ -20,18 +21,33 @@ export default function CastCarousel({
     onNextButtonClick,
   } = usePrevNextButtons(emblaApi);
 
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia("(max-width: 1050px)").matches
+  );
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 1050px)");
+    const handler = (e) => setIsMobile(e.matches);
+    mediaQuery.addEventListener("change", handler);
+    return () => mediaQuery.removeEventListener("change", handler);
+  }, []);
+
   return (
     <section className="relative overflow-visible z-10">
-      <PrevButton
-        disabled={prevBtnDisabled}
-        onClick={onPrevButtonClick}
-        className="absolute -left-6 top-1/2 -translate-y-1/2 -translate-x-1/2"
-      />
-      <NextButton
-        disabled={nextBtnDisabled}
-        onClick={onNextButtonClick}
-        className="absolute -right-6 top-1/2 -translate-y-1/2 translate-x-1/2"
-      />
+      {!isMobile && (
+        <PrevButton
+          disabled={prevBtnDisabled}
+          onClick={onPrevButtonClick}
+          className="absolute -left-6 top-1/2 -translate-y-1/2 -translate-x-1/2"
+        />
+      )}
+      {!isMobile && (
+        <NextButton
+          disabled={nextBtnDisabled}
+          onClick={onNextButtonClick}
+          className="absolute -right-6 top-1/2 -translate-y-1/2 translate-x-1/2"
+        />
+      )}
 
       <div ref={emblaRef} className="overflow-hidden w-full">
         <div className="flex gap-4 px-2">
--- src/features/movie/CastSection.jsx
+++ src/features/movie/CastSection.jsx
@@ -2,8 +2,8 @@ import CastCarousel from "./CastCarousel";
 
 function CastSection({ slides }) {
   return (
-    <div className="mx-[calc(2rem+2vw)] md:mx-[calc(2rem+3vw)] pb-14 px-6">
-      <h3 className="text-3xl mb-2">CAST</h3>
+    <div className="sm:mx-[calc(2rem+2vw)] md:mx-[calc(2rem+3vw)] lg:pb-14 px-6">
+      <h3 className="text-2xl lg:text-3xl mb-2 ml-1">CAST</h3>
       <CastCarousel
         slides={slides}
         options={{ dragFree: true }}
