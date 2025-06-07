7f34fe428ce0d2acaecac28aedee23d0367b4e03
Toma Milićević <tomatomamilicevic@gmail.com>
2025-06-07 22:31:57 +0200

    Refactor RecentlyWatched component for improved layout; update movie fetch limit and enhance responsiveness with better styling

--- src/features/profile/RecentlyWatched.jsx
+++ src/features/profile/RecentlyWatched.jsx
@@ -6,19 +6,19 @@ export default function RecentlyWatched() {
   const { profile } = useCurrentUser();
   const userId = profile?.id;
 
-  const { data: movies = [], isLoading } = useRecentlyWatched(userId, 4);
+  const { data: movies = [], isLoading } = useRecentlyWatched(userId, 5);
 
   if (isLoading) return null;
 
   return (
-    <section className="sm:max-w-6xl mx-auto mt-22 pb-10 text-siva-100">
-      <h3 className="text-2xl sm:text-3xl font-normal mb-4 sm:mb-6 ml-4 sm:ml-9">
+    <section className="sm:w-xl md:w-3xl xl:w-5xl sm:mx-auto mt-22 pb-10 text-siva-100 px-4 sm:px-0">
+      <h3 className="text-2xl sm:text-3xl font-normal mb-4 sm:mb-6 ">
         RECENTLY WATCHED
       </h3>
 
-      <div className="flex justify-around gap-3 px-4 sm:px-0 lg:gap-6 flex-nowrap sm:flex-wrap overflow-auto">
+      <div className="flex flex-nowrap gap-3 overflow-x-auto scroll-smooth snap-x snap-mandatory xl:grid xl:gap-4 xl:grid-cols-[repeat(auto-fill,minmax(160px,1fr))] xl:overflow-x-visible xl:snap-none min-w-full">
         {movies.map((m) => (
-          <div>
+          <div className="flex-none snap-start w-36 xl:flex-auto xl:w-auto">
             <MovieCard key={m.id} movie={m} hideActions />
           </div>
         ))}
