de0ce0d377038bc3e25f37851fdafc9ba79e0e7a
Toma Milićević <tomatomamilicevic@gmail.com>
2026-04-08 00:13:30 +0200

    disable vercel helpers

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,11 @@ import "./styles/index.css";
 // import { Analytics } from "@vercel/analytics/next";
 // import { SpeedInsights } from "@vercel/speed-insights/next";
 
-import { injectSpeedInsights } from "@vercel/speed-insights";
-import { inject } from "@vercel/analytics";
+// import { injectSpeedInsights } from "@vercel/speed-insights";
+// import { inject } from "@vercel/analytics";
 
-inject();
-injectSpeedInsights();
+// inject();
+// injectSpeedInsights();
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 0 } },
@@ -27,5 +27,5 @@ createRoot(document.getElementById("root")).render(
       </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
